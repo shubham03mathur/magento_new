b0c136e7364e21785a024fce5eff4b1a116b281b
shubham03mathur <shubham@excellencetechnologies.in>
2016-05-03 12:20:35 +0530

    Final_changes

---
+++ Block/Main.php
@@ -0,0 +1,40 @@
+<?php
+namespace Excellence\Table\Block;
+  
+class Main extends \Magento\Framework\View\Element\Template
+{   
+    protected $_testFactory;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Excellence\Table\Model\TestFactory $testFactory
+    )
+    {
+        $this->_testFactory = $testFactory;
+        parent::__construct($context);
+    }
+    protected function _prepareLayout()
+    {
+       
+        $test = $this->_testFactory->create()->getCollection();
+        // $test->setTitle('Test Title'); 
+        // $test->setEmail('shubham3mathur@gmail.com');
+        // $test->save();
+        $this->setTestModel($test);
+
+    }
+	public function getEditUrl($id)
+    {
+        $editUrl = "excellence/index/edit/id/".$id;
+        return $this->_urlBuilder->getUrl($editUrl);
+    }
+    public function getAddUrl()
+    {
+        return $this->_urlBuilder->getUrl("excellence/index/Add");
+    }
+    public function getDeleteUrl($id)
+    {
+        $deleteUrl = "excellence/index/delete/id/".$id;
+        return $this->_urlBuilder->getUrl($deleteUrl);
+    }
+}
+?>
